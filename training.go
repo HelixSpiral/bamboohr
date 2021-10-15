31e3fa73505457bd376cb0948fca409d65b1129a
Shawn Smith <ShawnSmith0828@gmail.com>
2021-10-14 22:29:43 -0400

    Adding some functionality for trainings
    Also adding a LICENSE file

---
+++ training.go
@@ -0,0 +1,57 @@
+package bamboohr
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// ListTrainingTypes lists the types of training your company has
+func (b *Client) ListTrainingTypes() ([]byte, error) {
+	endpointURL := fmt.Sprintf("%s/training/type", b.APIEndpoint)
+
+	req, err := http.NewRequest("GET", endpointURL, nil)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	resp, err := b.sendRequest(req)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return resp, nil
+}
+
+// ListTrainingCategories lists the training categories your company has
+func (b *Client) ListTrainingCategories() ([]byte, error) {
+	endpointURL := fmt.Sprintf("%s/training/category", b.APIEndpoint)
+
+	req, err := http.NewRequest("GET", endpointURL, nil)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	resp, err := b.sendRequest(req)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return resp, nil
+}
+
+// ListEmployeeTrainings lists the trainings by an employee
+func (b *Client) ListEmployeeTrainings(id string) ([]byte, error) {
+	endpointURL := fmt.Sprintf("%s/training/record/employee/%s", b.APIEndpoint, id)
+
+	req, err := http.NewRequest("GET", endpointURL, nil)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	resp, err := b.sendRequest(req)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return resp, nil
+}
