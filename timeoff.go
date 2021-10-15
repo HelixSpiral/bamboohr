7b931639b24bd6db85dfaabe643d8ea9b491d04b
Shawn Smith <ShawnSmith0828@gmail.com>
2021-10-14 22:59:16 -0400

    A small timeoff fix + new functions and some training additions

--- timeoff.go
+++ timeoff.go
@@ -44,6 +44,43 @@ func (b *Client) ListTimeOffPolicies() ([]byte, error) {
 	return resp, nil
 }
 
+// ListTimeOffRequests lists the timeoff requests
+func (b *Client) ListTimeOffRequests(start, end time.Time, args map[string]interface{}) ([]byte, error) {
+	endpointURL := fmt.Sprintf("%s/time_off/requests/?start=%s&end=%s", b.APIEndpoint, start.Format("2006-01-02"), end.Format("2006-01-02"))
+
+	if id, ok := args["id"]; ok {
+		endpointURL += fmt.Sprintf("&id=%s", id)
+	}
+
+	if action, ok := args["action"]; ok {
+		endpointURL += fmt.Sprintf("&action=%s", action)
+	}
+
+	if empId, ok := args["employeeid"]; ok {
+		endpointURL += fmt.Sprintf("&employeeId=%s", empId)
+	}
+
+	if tType, ok := args["type"]; ok {
+		endpointURL += fmt.Sprintf("&type=%s", tType)
+	}
+
+	if status, ok := args["status"]; ok {
+		endpointURL += fmt.Sprintf("&status=%s", status)
+	}
+
+	req, err := http.NewRequest("GET", endpointURL, nil)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	resp, err := b.sendRequest(req)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return resp, nil
+}
+
 // ListTimeOffPoliciesForEmployee lists the time off policies for a specific employee
 func (b *Client) ListTimeOffPoliciesForEmployee(id string) ([]byte, error) {
 	endpointURL := fmt.Sprintf("%s/employees/%s/time_off/policies", b.APIEndpoint, id)
@@ -81,11 +118,11 @@ func (b *Client) EstimateFutureTimeOffBalance(id string, end time.Time) ([]byte,
 // ListWhosOut lists the employees out for the specified dates
 func (b *Client) ListWhosOut(args map[string]time.Time) ([]byte, error) {
 	endpointURL := fmt.Sprintf("%s/time_off/whos_out/", b.APIEndpoint)
-	if startDate, ok := args["start"]; !ok {
+	if startDate, ok := args["start"]; ok {
 		endpointURL += fmt.Sprintf("?start=%s", startDate.Format("2006-01-02"))
 	}
 
-	if endDate, ok := args["end"]; !ok {
+	if endDate, ok := args["end"]; ok {
 		endpointURL += fmt.Sprintf("&end=%s", endDate.Format("2006-01-02"))
 	}
 
--- training.go
+++ training.go
@@ -39,6 +39,23 @@ func (b *Client) ListTrainingCategories() ([]byte, error) {
 	return resp, nil
 }
 
+// ListEmployeeTrainingsByType lists the training of an employee for a specific type
+func (b *Client) ListEmployeeTrainingsByType(id, tType string) ([]byte, error) {
+	endpointURL := fmt.Sprintf("%s/training/record/employee/%s/type/%s", b.APIEndpoint, id, tType)
+
+	req, err := http.NewRequest("GET", endpointURL, nil)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	resp, err := b.sendRequest(req)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return resp, nil
+}
+
 // ListEmployeeTrainings lists the trainings by an employee
 func (b *Client) ListEmployeeTrainings(id string) ([]byte, error) {
 	endpointURL := fmt.Sprintf("%s/training/record/employee/%s", b.APIEndpoint, id)
